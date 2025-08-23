40222baa53fd8befbc9d8b02a7029dcacd678bec
Hemant Mehra <mehra.hemant14@gmail.com>
2025-08-23 21:54:54 +0530

    fix posts views

--- posts/views.py
+++ posts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def index(request):
     context = {'page_title': 'Welcome to my Django App'}
-    return render(request, 'my_app/index.html', context)
+    return render(request, 'posts/index.html', context)
